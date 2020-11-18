8a2b2a229a5d2082ea983179a641c5c05d32bd1d
prashantrmishra <pm7044872@gmail.com>
2020-11-18 11:51:51 +0530

    tiles added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bhojpuriMirrorClient';
+
+  tiles: Tile[] = [{ text: 'tile one', cols: 3, rows: 4, color: 'lightblue' },
+  { text: 'tile 2', cols: 1, rows: 2, color: 'lightblue' },
+  { text: 'tile 3', cols: 1, rows: 1, color: 'lightblue' },
+  { text: 'tile 4', cols: 1, rows: 1, color: 'lightblue' }]
+
+  sportTiles: Tile[] = [{ text: 'tile 1', cols: 1, rows: 2, color: 'lightblue' },
+  { text: 'tile 2', cols: 2, rows: 4, color: 'lightblue' },
+  { text: 'tile 3', cols: 1, rows: 4, color: 'lightblue' },
+  { text: 'tile 4', cols: 1, rows: 2, color: 'lightblue' }]
+}
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
